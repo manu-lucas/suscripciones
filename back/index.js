5e80bddfd30fe72c9b04f317929426148720712a
manu-lucas <lmechegaray@wecom.global>
2024-05-31 17:33:43 -0300

    changes

--- back/index.js
+++ back/index.js
@@ -57,17 +57,54 @@ app.post("/create_preference", async (req, res) => {
 
 app.post("/process_payment", (req, res) => {
 
-  const idClient = req.query.idClient;
-  const planId = req.query.planId;
-  const { formData } = req.body;
-
-  
+const idClient = req.query.idClient;
+const planId = req.query.planId;
+const {payer,token,transaction_amount} = req.body
   console.log(planId, idClient)
   console.log(req.body)
+console.log(payer,token,transaction_amount)
 
+
+  // const data = {
+  //   "preapproval_plan_id": planId,
+  //   "reason": "Programacion Basica",
+  //   "external_reference": "YG-001",
+  //   "payer_email": payer.email,
+  //   "card_token_id": token,
+  //   "auto_recurring": {
+  //     "frequency": 1,
+  //     "frequency_type": "months",
+  //     "start_date": "2024-06-01T13:07:14.260Z",
+  //     "end_date": "2025-12-01T15:59:52.581Z",
+  //     "transaction_amount": transaction_amount,
+  //     "currency_id": "ARS"
+  //   },
+  //   "back_url": "https://chronic-compiler-systematic-consequence.trycloudflare.com/pagoexitoso",
+  //   "status": "authorized"
+  // };
+
+
+  //   const generarSuscripcion = ()=>{
+  //     axios.post('https://api.mercadopago.com/preapproval', data, {
+  //       headers: {
+  //         'Authorization': 'Bearer TEST-3926537940861188-052208-5ca7262b6d68d1b35b9a052615a0b8f2-1796796862',
+  //         'Content-Type': 'application/json'
+  //       }
+  //     })
+  //     .then((response) => {
+  //       console.log('Respuesta:', response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error.response.data);
+  //     });
+    
+  //   }
+  //   if(token){
+  //     generarSuscripcion()
+
+  //   }
+    
   res.status(200).send({success:true})
-
-
 
 });
 
@@ -97,46 +134,7 @@ app.get("/ver", async (req, res) => {
   }
 });
 
-//
-// const client = new MercadoPago({ accessToken: '<ACCESS_TOKEN>', options: { timeout: 5000 } });
 
-// const payment = new Payment(client);
-
-// payment.get({
-// 	id: '<PAYMENT_ID>',
-// }).then(console.log).catch(console.log);
-
-// app.post("/create_preference", async (req, res) => {
-//   const preference = new Preference(client);
-
-//   preference
-//     .create({
-//       body: {
-//         items: [
-//           {
-//             id: "item-ID-1",
-//             title: req.body.title,
-//             quantity: req.body.quantity,
-//             unit_price: req.body.price,
-//             currency_id: "ARS",
-//           },
-//         ],
-//         back_urls: {
-//           success: "https://web-empresa-kappa.vercel.app/",
-//           failure: "https://web-empresa-kappa.vercel.app/",
-//           pending: "https://web-empresa-kappa.vercel.app/",
-//         },
-//         marketplace_fee: 1,
-//       },
-//     })
-//     .then((result) => {
-//       const respuesta = result;
-//       console.log(respuesta.id);
-
-//       res.status(200).json({ id: respuesta.id });
-//     })
-//     .catch(console.log);
-// });
 
 app.listen(port, () => {
   console.log("servidor escuchando en el puerto 3000");
